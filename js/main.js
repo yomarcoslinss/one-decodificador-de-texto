ec0cdd9a1bb7c52a10eb0ab838f8f74b40aa86ed
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-04-22 18:06:14 -0300

    feat: função do botão descriptografar implementada

--- js/main.js
+++ js/main.js
@@ -12,11 +12,20 @@ function criptografar() {
         elementosOcultos[i].style.display = "none";
     }
 
-    entradaOperacao = selecionarTextArea.value;    
+    entradaOperacao = selecionarTextArea.value;
+    
+    entradaOperacao = entradaOperacao.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat"); 
 
-    boxSaida.innerHTML = entradaOperacao.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+    boxSaida.innerHTML = entradaOperacao; 
 
+}
+
+function descriptografar() {
+
+    boxSaida.innerHTML = entradaOperacao.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    
 }
 
 
 btnCriptografar.onclick = criptografar;
+btnDescriptografar.onclick = descriptografar;
