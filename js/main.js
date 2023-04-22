13c88b08d65301e474be78c7881e25cc34f37045
yomarcoslinss <vinicius.gzm0@gmail.com>
2023-04-22 20:10:10 -0300

    fix: bug na responsividade da saída e bug da função 'descriptografar' corrigidos

--- js/main.js
+++ js/main.js
@@ -3,27 +3,44 @@ var btnDescriptografar = document.querySelector("#btn-descriptografar");
 var elementosOcultos = document.querySelectorAll(".ocultar");
 var selecionarTextArea = document.querySelector("#entrada-texto");
 var entradaOperacao = "";
-var boxSaida = document.querySelector("#saida-operacao");
+var saidaOperacao = document.querySelector("#saida-operacao");
 
 
 function criptografar() {
 
-    for(i = 0; i < elementosOcultos.length; i++) {
+    for (i = 0; i < elementosOcultos.length; i++) {
         elementosOcultos[i].style.display = "none";
     }
 
-    entradaOperacao = selecionarTextArea.value;
-    
-    entradaOperacao = entradaOperacao.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat"); 
+    var boxSaida = document.querySelector("#box-saida")
+    boxSaida.style.justifyContent = "flex-start"
+    boxSaida.style.textAlign = "start";
 
-    boxSaida.innerHTML = entradaOperacao; 
+    entradaOperacao = selecionarTextArea.value;
+
+    entradaOperacao = entradaOperacao.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+
+    saidaOperacao.innerHTML = entradaOperacao;
 
 }
 
 function descriptografar() {
 
-    boxSaida.innerHTML = entradaOperacao.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
-    
+    for (i = 0; i < elementosOcultos.length; i++) {
+        elementosOcultos[i].style.display = "none";
+    }
+
+    var boxSaida = document.querySelector("#box-saida")
+    boxSaida.style.justifyContent = "flex-start"
+    boxSaida.style.textAlign = "start";
+
+
+    entradaOperacao = selecionarTextArea.value;
+
+    entradaOperacao = entradaOperacao.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+    saidaOperacao.innerHTML = entradaOperacao;
+
 }
 
 
